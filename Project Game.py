8bfe461722efcf935d9c63a4e2fc87786c28d261
wellandbb <43852743+wellandbb@users.noreply.github.com>
2019-06-13 10:06:58 +0100

    Add files via upload

--- Project Game.py
+++ Project Game.py
@@ -1,8 +1,15 @@
 # import pygame
-import pygame, sys
+import pygame, sys, random
 
 # initalise pygame
 pygame.init() 
+
+#window
+screenWidth = 700
+screenHeight = 500
+screen = (screenWidth,screenHeight)
+win = pygame.display.set_mode(screen)
+
 
 # intialise some colours
 red = (255, 0, 0)
@@ -10,6 +17,10 @@ blue = (0, 0, 255)
 green = (0, 255, 0)
 black = (0,0,0)
 white = (255,255,255)
+
+# text set up
+pygame.font.init()
+myfont = pygame.font.SysFont('Arial', 50)
 
 # setting a caption for game window
 pygame.display.set_caption('Game Project')
@@ -23,10 +34,7 @@ class player(pygame.sprite.Sprite):
     #initialisation
     def __init__ (self, x, y, width, height):
         super().__init__()
-        self.x = x
-        self.y = y
-        self.width = width
-        self.height = height
+        self.rect = pygame.Rect(x, y, width, height)
         self.vel = 5
         self.left = False
         self.right = False
@@ -35,15 +43,15 @@ class player(pygame.sprite.Sprite):
 
      #draw function
     def draw(self,win):
-        pygame.draw.rect(win, red, (self.x, self.y, self.width, self.height))
+        pygame.draw.rect(win, red, self.rect)
    
 # bullet class
 class projectile(pygame.sprite.Sprite):
-    def __init__(self,x,y,height, width,colour,facing, XorY):
+    def __init__(self,x,y,height, width,colour,facing, XorY, velocity):
         super().__init__()
         self.rect = pygame.Rect(x,y,width,height)
         self.colour = colour
-        self.vel = 8 * facing
+        self.vel = velocity * facing
         if XorY == "X":
             self.movingX = True
             self.movingY = False
@@ -58,13 +66,11 @@ class projectile(pygame.sprite.Sprite):
 # enemy class
 class enemy(pygame.sprite.Sprite):
 
-    def __init__(self,x,y,width,height,end):
+    def __init__(self,x,y,width,height):
         super().__init__()
         self.rect = pygame.Rect(x,y,width,height)
-        self.vel = 3
-        #path
-        self.end = end
-        self.path = [self.rect.x, self.end]
+        self.vel = 1
+        self.health = 3
 
     #draw function
     def draw(self,win):
@@ -73,19 +79,30 @@ class enemy(pygame.sprite.Sprite):
 
     #movement
     def move(self):
-        if self.vel > 0:
-            if self.rect.x +self.vel < self.path[1]:
-                self.rect.x += self.vel
-            else:
-                self.vel = self.vel * -1
-        else:
-            if self.rect.x - self.vel > self.path[0]:
-                self.rect.x += self.vel
+        if self.rect.x < man.rect.x:
+            self.rect.x += self.vel
+        elif self.rect.x > man.rect.x:
+            self.rect.x -= self.vel
 
-            else:
-                self.vel = self.vel * -1
-        pass
-     
+        if self.rect.y < man.rect.y:
+            self.rect.y += self.vel
+        elif self.rect.y > man.rect.y:
+            self.rect.y -= self.vel
+    
+    def shot(self):
+        self.health -= 1
+        if self.health == 0:
+            enemies.remove(self)
+        enemy_hit_list.remove(self)
+
+# game over function
+def gameOver():
+    win.blit(myfont.render('Game Over', True, white), (screenWidth/2 - 125, 50))
+
+# game win funtion
+def gameWin():
+    win.blit(myfont.render('Game Win', True, white), (screenWidth/2 - 125, 50))
+
 #intialise objects
 all_sprite_list = pygame.sprite.Group()
 
@@ -95,26 +112,22 @@ all_sprite_list.add(man)
 
 #enemies
 enemies = pygame.sprite.Group()
-evil = enemy(200, 250, 20, 20, 500)
-enemies.add(evil)
-all_sprite_list.add(evil)
+for i in range(10):
+    i = enemy(random.randint(10,screenWidth-30),random.randint(10,screenHeight/2), 20, 20)
+    enemies.add(i)
+    all_sprite_list.add(i)
 
 #projectiles
 bullets = pygame.sprite.Group()
-
-#window
-screenWidth = 700
-screenHeight = 500
-screen = (screenWidth,screenHeight)
-win = pygame.display.set_mode(screen)
-
+reloadTime = 0
 
 # draw game function
-def redrawGameWindow() :
+def redrawGameWindow():
 
     man.draw(win)
     for enemy in enemies:
         enemy.draw(win)
+
     for bullet in bullets:
         bullet.draw(win)
     #display update window
@@ -135,6 +148,11 @@ while run:
         if event.type == pygame.QUIT:
             run = False
     
+    # check if there are no enemies
+    if enemies.__len__() == 0:
+        gameWin()
+        run = False
+    
     # bullets
     for bullet in bullets:
         if bullet.movingX:
@@ -142,18 +160,43 @@ while run:
                 bullet.rect.x += bullet.vel
             else:
                 bullets.remove(bullet)
+        
+        # changes bullet dimensions
         if bullet.movingY:
+            temp = bullet.rect.width
+            bullet.rect.width = bullet.rect.height
+            bullet.rect.height = bullet.rect.width
             if bullet.rect.y < screenHeight and bullet.rect.y > 0:
                 bullet.rect.y += bullet.vel
             else:
                 bullets.remove(bullet)
+       
+       # bullet-enemy collisions
         enemy_hit_list = pygame.sprite.spritecollide(bullet, enemies, True)
         for enemy in enemy_hit_list:
             bullets.remove(bullet)
-            enemies.remove(enemy)
+            if bullet.movingX:
+                if bullet.vel > 0:
+                    enemy.rect.x += 3
+                else:
+                    enemy.rect.x -= 3
+            else:
+                if bullet.vel > 0:
+                    enemy.rect.y += 3
+                else:
+                    enemy.rect.y -= 3                
+            enemy.shot()
+
+    # enemy-player collisions
+    for enemy in enemies:
+        if pygame.sprite.collide_rect(enemy, man):
+            gameOver()
+            run = False
+
 
     # key press events
     keys = pygame.key.get_pressed()
+
     #shooting
     if keys[pygame.K_SPACE]:
         # determines which way facing
@@ -165,32 +208,35 @@ while run:
             XorY = "X"
         else:
             XorY = "Y"
-        # max 5 bullets on screen at a time, makes bullet at centre of player and moves in direction facing
-        if len(bullets) < 5:
-           bullets.add(projectile(round(man.x + man.width//4),  round(man.y + man.height//4), 10, 10, blue, facing, XorY))
+        # max 5 bullets on screen at a time, makes bullet at centre of player and moves in direction facing, also adds delay between bullets
+        if len(bullets) < 5 and reloadTime == 0:
+           bullets.add(projectile(round(man.rect.x + man.rect.width//4),  round(man.rect.y + man.rect.height//4), 6, 10, blue, facing, XorY, 10))
+           reloadTime = 1
+        else:
+            reloadTime = 0
 
 
     #movement key presses with facing direction for shooting
     if keys[pygame.K_UP]:
-        man.y -= man.vel
+        man.rect.y -= man.vel
         man.up = True
         man.left = False
         man.right = False
         man.down = False
     elif keys[pygame.K_DOWN]:
-        man.y += man.vel
+        man.rect.y += man.vel
         man.up = False
         man.left = False
         man.down = True
         man.right = False
     elif keys[pygame.K_LEFT]:
-        man.x -= man.vel
+        man.rect.x -= man.vel
         man.left = True
         man.down = False
         man.right = False
         man.up = False
     elif keys[pygame.K_RIGHT]:
-        man.x += man.vel
+        man.rect.x += man.vel
         man.left = False
         man.up = False
         man.down = False
@@ -198,6 +244,16 @@ while run:
 
     # display loop updates
     redrawGameWindow()
+
+while not run:
+
+    clock.tick(30)
+
+    #event loop
+    for event in pygame.event.get():
+        # bomb out of loop if quit
+        if event.type == pygame.QUIT:
+            run = True
 
 #quit program
 pygame.quit()
